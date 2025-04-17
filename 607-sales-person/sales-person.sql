e49000f27bf6e00a48d543b66b482a6cf30e9b88
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-04-16 20:32:54 -0500

    Time: 1351 ms (88.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+
+select 
+name
+from 
+salesperson
+where sales_id 
+not in
+(select sales_id from orders o left join company c on o.com_id = c.com_id
+where c.name = 'RED')
+
