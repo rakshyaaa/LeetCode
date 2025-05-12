02a40d03a74cbbe0d8768f203b9d863b0da25d07
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-05-12 15:25:09 -0500

    Time: 432 ms (76.78%) | Memory: 0B (100.00%) - LeetSync

--- 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
+++ 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
@@ -1,11 +1,4 @@
 # Write your MySQL query statement below
 
-with cte as (
-select 
-delivery_id,
-case when order_date = customer_pref_delivery_date  then 'immediate'
-else 'scheduled' end as delivery_name
-from delivery group by delivery_id
-) 
-select round(sum(case when delivery_name ='immediate' then 1 else 0 end)/ count(distinct delivery_id) * 100,2) as immediate_percentage
-from cte;
+select
+round(100* avg(order_date = customer_pref_delivery_date), 2) as immediate_percentage from delivery;
