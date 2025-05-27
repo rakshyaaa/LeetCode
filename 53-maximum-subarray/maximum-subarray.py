3e1134763a838fd70f5d198dace9bd7509bbb4a4
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-05-27 06:57:27 -0500

    Time: 22 ms (97.94%) | Memory: 21.9 MB (38.82%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,16 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
         maxSub = nums[0]
+
         currSum = 0
 
         for n in nums:
-            if currSum <= 0:
+            if currSum < 0:
                 currSum = 0
-           
+            
             currSum += n
 
             if currSum > maxSub:
-                maxSub =  currSum
-        
+                maxSub = currSum
+
         return maxSub
