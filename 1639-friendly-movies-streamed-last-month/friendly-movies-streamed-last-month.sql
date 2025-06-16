9877403c208673cb9195d6117f554a44473e74b7
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-06-16 06:40:45 -0500

    Time: 926 ms (61.65%) | Memory: 0B (100.00%) - LeetSync

--- 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
+++ 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
@@ -4,4 +4,4 @@ select distinct c.title
 from TVProgram t
 join content c
 on t.content_id = c.content_id where c.Kids_content = 'Y' and c.content_type = 'Movies'
-and year(program_date) = 2020 and month(program_date) = 6
+and t.program_date between '2020-06-01 00:00' and '2020-06-30 23:59' 
