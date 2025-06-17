de12171b29c4465299c56fc6106acbbe20b26af6
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-06-17 06:41:13 -0500

    Time: 397 ms (51.73%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -4,8 +4,8 @@ with cte as (
 select 
 seat_id,
 free,
-LAG(free,1) over (order by seat_id) as prev_free,
-LEAD(free,1) over (order by seat_id) as next_free
+LAG(free) over (order by seat_id) as prev_free,
+LEAD(free) over (order by seat_id) as next_free
 from cinema
 ) select seat_id from cte where free = 1 and (prev_free = 1 or next_free = 1)
 order by seat_id
