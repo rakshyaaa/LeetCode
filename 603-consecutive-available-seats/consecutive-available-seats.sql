ca2296872a27d3935c49263cea2b8c1f882508a2
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-06-17 06:25:56 -0500

    Time: 478 ms (8.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+with cte as (
+select 
+seat_id,
+free,
+LAG(free,1) over (order by seat_id) as prev_free,
+LEAD(free,1) over (order by seat_id) as next_free
+from cinema
+) select seat_id from cte where free = 1 and (prev_free = 1 or next_free = 1)
+order by seat_id
