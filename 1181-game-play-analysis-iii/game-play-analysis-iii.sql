c4ed44db0a609202be949536a6eccd172507201b
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-06-21 18:12:20 -0500

    Time: 593 ms (91.82%) | Memory: 0B (100.00%) - LeetSync

--- 1181-game-play-analysis-iii/game-play-analysis-iii.sql
+++ 1181-game-play-analysis-iii/game-play-analysis-iii.sql
@@ -3,5 +3,5 @@
 select 
 player_id, 
 event_date,
-sum(games_played) over (partition by player_id order by event_date) as games_played_so_far
+sum(games_played) over (partition by player_id order by event_date ROWS UNBOUNDED PRECEDING) as games_played_so_far
 from activity;
