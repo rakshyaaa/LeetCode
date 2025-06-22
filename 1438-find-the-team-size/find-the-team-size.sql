04dd567722491833e975c3e86c8cda60db32305e
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-06-22 12:03:06 -0500

    Time: 277 ms (75.14%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1438-find-the-team-size/find-the-team-size.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+select employee_id, team_size from employee e
+join
+(select team_id, count(team_id) as team_size from employee
+group by team_id) t
+on e.team_id = t.team_id
