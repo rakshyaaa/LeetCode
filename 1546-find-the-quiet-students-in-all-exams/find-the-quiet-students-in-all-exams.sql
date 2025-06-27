d3e9c26ebe43fdde320943842b93864a553feac9
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2025-06-27 06:19:31 -0500

    Time: 573 ms (27.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
@@ -0,0 +1,26 @@
+# Write your MySQL query statement below
+
+
+with cte as (
+    select exam_id,
+    max(score) as high_score, 
+    min(score) as low_score 
+    from exam 
+    group by exam_id
+),
+cte2 as (
+select 
+s.student_id, 
+s.student_name,
+e.exam_id,
+e.score
+from student s
+left join exam e
+on s.student_id = e.student_id
+left join cte c
+on c.exam_id = e.exam_id
+where score in (high_score, low_score) or score is null
+)
+select student_id, student_name from student where student_id not in (select student_id from cte2)
+
+
